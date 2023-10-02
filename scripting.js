b11aab1d1028a18d2481d4f1d64e10bb6f3def36
PierceVDR <piercevdr@gmail.com>
2023-10-02 14:47:20 -0400

    sum

--- scripting.js
+++ scripting.js
@@ -1,7 +1,17 @@
+btn = document.getElementById("btn")
+
 die1 = document.getElementById("die1");
 die2 = document.getElementById("die2");
 
+sumDiv = document.getElementById("sumDiv")
+
+
 function roll() {
-    die1.innerHTML=Math.floor(Math.random()*6 + 1)
-    die2.innerHTML=Math.floor(Math.random()*6 + 1)
+    one=Math.floor(Math.random()*6 + 1);
+    two=Math.floor(Math.random()*6 + 1);
+
+    die1.innerHTML=one
+    die2.innerHTML=two
+
+    sumDiv.innerHTML="Sum: " + (one+two);
 }
